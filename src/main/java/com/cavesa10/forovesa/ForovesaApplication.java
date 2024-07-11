c6a34636d58747b28ae261d9683b4869d28337f1
cavesa10 <cavesa10@gmail.com>
2024-07-11 13:00:54 -0500

    crear repositorio

---
+++ src/main/java/com/cavesa10/forovesa/ForovesaApplication.java
@@ -0,0 +1,13 @@
+package com.cavesa10.forovesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForovesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForovesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cavesa10/forovesa/ForovesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavesa10.forovesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForovesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
